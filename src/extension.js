cd1007b20dcaa1e31e6ad18c0cae26b67f568cd4
Philippe Troin <phil+github-commits@fifi.org>
2019-02-08 22:46:44 -0800

    Basic extension.

---
+++ src/extension.js
@@ -0,0 +1,34 @@
+// Tweaks-status - Put Gnome Tweaks on ALT/long-press on the panel's Settings
+// Copyright (C) 2019 Philippe Troin <phil@fifi.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const Logger = Me.imports.logger;
+
+let logger = new Logger.Logger('Tweaks-Status');
+logger.set_debug(true);
+
+function init() {
+}
+
+function enable() {
+    logger.log('enable()');
+}
+
+function disable() {
+    logger.log('disable()');
+}

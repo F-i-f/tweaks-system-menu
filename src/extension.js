159f8147721e7b15204208af113e723b55e60d1c
Philippe Troin <phil+github-commits@fifi.org>
2019-02-10 17:05:09 -0800

    Eliminate global variables.
    Delay initialization of anything until enable() time.
    Integrate position setting.

--- src/extension.js
+++ src/extension.js
@@ -21,11 +21,9 @@ const Shell = imports.gi.Shell;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 
 const Logger = Me.imports.logger;
-
-let logger = new Logger.Logger('Tweaks-System-Menu');
-logger.set_debug(true);
 
 const TweaksSystemMenuActionButton = new Lang.Class({
     Name: 'TweaksSystemMenuActionButton',
@@ -39,16 +37,19 @@ const TweaksSystemMenuActionButton = new Lang.Class({
     },
 
     _log_debug: function(msg) {
-	logger.log_debug('TweaksSystemMenuActionButton(' + this._appName + '): '+msg);
+	this._extension._logger.log_debug('TweaksSystemMenuActionButton('
+					  + this._appName + '): '+msg);
     },
 
     enable: function() {
 	this._log_debug('enable()');
 
-	this._app    = Shell.AppSystem.get_default().lookup_app( this._appName );
-	this._action = this._extension._systemMenu._createActionButton(this._app.app_info.get_icon().names[0],
-								       this._app.get_name());
-	this._signalConnection = this._action.connect('clicked', this._on_clicked.bind(this));
+	this._app    = Shell.AppSystem.get_default().lookup_app(this._appName);
+	this._action = this._extension._systemMenu
+	    ._createActionButton(this._app.app_info.get_icon().names[0],
+				 this._app.get_name());
+	this._signalConnection = this._action.connect('clicked',
+						      this._on_clicked.bind(this));
     },
 
     disable: function(destroy) {
@@ -84,11 +85,19 @@ const TweaksSystemMenuExtension = new Lang.Class({
     Name: 'TweaksSystemMenuExtension',
 
     _init: function() {
+	this._logger = null;
+
+	this._settings = null;
+	this._debugSettingChangedConnection = null;
+	this._positionSettingChangedConnection = null;
+
 	this._systemMenu = null;
 
+	this._areButtonsMerged = null;
 	this._tweaksButton = null;
 	this._settingsButton = null;
 	this._settingsSwitcher = null;
+	this._actorToPosition = null;
 	this._openStateChangedConnectionId = null;
     },
 
@@ -96,58 +105,67 @@ const TweaksSystemMenuExtension = new Lang.Class({
 	let systemActions = this._systemMenu._actionsItem.actor.get_children();
 	for (let i=0; i < systemActions.length; ++i) {
 	    if (systemActions[i] == action) {
-		logger.log_debug('_findSystemAction('+action+') = '+i);
+		this._logger.log_debug('_findSystemAction('+action+') = '+i);
 		return i;
 	    }
 	}
-	logger.log_debug('_findSystemAction('+action+') = <null>');
+	this._logger.log_debug('_findSystemAction('+action+') = <null>');
 	return null;
     },
 
     enable: function() {
-	logger.log_debug('enable()');
+	this._logger = new Logger.Logger('Tweaks-System-Menu');
+	this._settings = Convenience.getSettings();
+
+	this._on_debug_change();
+
+	this._logger.log_debug('enable()');
+
+	this._debugSettingChangedConnection = this._settings.connect('changed::debug', this._on_debug_change.bind(this));
+	this._positionSettingChangedConnection = this._settings.connect('changed::position', this._on_position_change.bind(this));
 
 	this._systemMenu = Main.panel.statusArea.aggregateMenu._system;
 
 	this._tweaksButton = new TweaksSystemMenuActionButton(this, 'org.gnome.tweaks.desktop');
 	this._tweaksButton.enable();
 
-	if (false) {
-	    this._systemMenu._actionsItem.actor.add(this._tweaksButton.getAction(), {expand: true, x_fill: false});
-	    this._systemMenu._actionsItem.actor.set_child_at_index(this._tweaksButton.getAction(), 2);
-	} else {
-	    let settingsActionIndex = this._findSystemAction(this._systemMenu._settingsAction);
-
+	if (this._settings.get_boolean('merge-with-settings')) {
 	    this._settingsButton = new TweaksSystemMenuActionButton(this, 'gnome-control-center.desktop');
 	    this._settingsButton.enable();
 
-	    this._settingsSwitcher = new StatusSystem.AltSwitcher(this._settingsButton.getAction(), this._tweaksButton.getAction());
+	    this._settingsSwitcher = new StatusSystem.AltSwitcher(this._settingsButton.getAction(),
+								  this._tweaksButton.getAction());
 
-	    this._systemMenu._actionsItem.actor.add(this._settingsSwitcher.actor, {expand:true, x_fill:false});
-	    this._systemMenu._actionsItem.actor.set_child_at_index(this._settingsSwitcher.actor, settingsActionIndex+1);
+	    this._systemMenu._actionsItem.actor.add(this._settingsSwitcher.actor,
+						    {expand:true, x_fill:false});
+	    this._actorToPosition = this._settingsSwitcher.actor;
+	    this._on_position_change();
 
 	    this._systemMenu._settingsAction.visible = false;
+	} else {
+	    this._systemMenu._actionsItem.actor.add(this._tweaksButton.getAction(),
+						    {expand: true, x_fill: false});
+	    this._actorToPosition = this._tweaksButton.getAction();
+	    this._on_position_change();
 	}
-	this._openStateChangedConnectionId = this._systemMenu.menu.connect('open-state-changed', this._on_open_state_changed.bind(this));
+	this._openStateChangedConnectionId = this._systemMenu.menu.connect('open-state-changed',
+									   this._on_open_state_changed.bind(this));
     },
 
     disable: function() {
-	logger.log_debug('disable()');
+	this._logger.log_debug('disable()');
 
 	this._systemMenu.menu.disconnect(this._openStateChangedConnectionId);
 	this._openStateChangedConnectionId = null;
 
-	if (false) {
-	    this._systemMenu._actionsItem.actor.remove_child(this._tweaksButton.getAction());
+	this._systemMenu._actionsItem.actor.remove_child(this._actorToPosition);
 
-	    this._tweaksButton.disable(true);
-	    this._tweaksButton = null;
-	} else {
-	    this._systemMenu._actionsItem.actor.remove_child(this._settingsSwitcher.actor);
+	this._actorToPosition = null;
 
-	    this._tweaksButton.disable(false);
-	    this._tweaksButton = null;
+	this._tweaksButton.disable(this._areButtonsMerged());
+	this._tweaksButton = null;
 
+	if (this._areButtonsMerged()) {
 	    this._settingsButton.disable(false);
 	    this._settingsButton = null;
 
@@ -158,10 +176,45 @@ const TweaksSystemMenuExtension = new Lang.Class({
 	}
 
 	this._systemMenu = null;
+
+	this._settings.disconnect(this._debugSettingChangedConnection);
+	this._debugSettingChangedConnection = null;
+	this._settings.disconnect(this._positionSettingChangedConnection);
+	this._positionSettingChangedConnection = null;
+
+	this._settings = null;
+
+	this._logger = null;
+    },
+
+    _areButtonsMerged: function() {
+	return this._settingsSwitcher == null;
+    },
+
+    _on_debug_change: function() {
+	this._logger.set_debug(this._settings.get_boolean('debug'));
+	this._logger.log_debug('debug = '+this._logger.get_debug());
+
+    },
+
+    _on_position_change: function() {
+	let position = this._settings.get_int('position');
+	this._logger.log_debug('_on_position_change(): settings position=' + position);
+	if (position == -1) {
+	    position = this._findSystemAction(this._systemMenu._settingsAction)+1;
+	    this._logger.log_debug('_on_position_change(): automatic position=' + position);
+	}
+	let n_children = this._systemMenu._actionsItem.actor.get_n_children();
+	if (position >= n_children) {
+	    position = n_children-1;
+	    this._logger.log_debug('_on_position_change(): adjusting position='
+				   + position + ' with '+n_children+' elements');
+	}
+	this._systemMenu._actionsItem.actor.set_child_at_index(this._actorToPosition, position);
     },
 
     _on_open_state_changed: function(menu, open) {
-	logger.log_debug('_on_open_state_changed()');
+	this._logger.log_debug('_on_open_state_changed()');
 	if (!open)
 	    return;
 	this._tweaksButton.setVisible(true);
